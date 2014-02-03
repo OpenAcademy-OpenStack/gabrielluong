1c6fef132e0dfc7ce2f143ddb16218f2446249ea
Gabriel Luong <gabriel.luong@gmail.com>
2014-02-03 00:10:20 -0500

    Added exercise 1

---
+++ ex1/createvm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+from novaclient.v1_1 import client as nclient
+from keystoneclient.v2_0 import client as kclient
+from credential import credentials
+from datetime import datetime
+import glanceclient as gclient
+
+
+# Authenticate against Keystone and return a valid Keystone client
+def getKeystoneClient():
+    return kclient.Client(username=credentials["username"],
+                          password=credentials["password"],
+                          auth_url=credentials["auth_url"],
+                          tenant_name=credentials["tenant_name"])
+
+
+# Returns a Glance client
+def getGlanceClient():
+    return gclient.Client(
+        "1", endpoint=keystone.service_catalog.url_for(service_type="image"),
+        token=keystone.auth_token)
+
+
+# Returns a Nova client
+def getNovaClient():
+    return nclient.Client(credentials["username"], credentials["password"],
+                          credentials["tenant_name"], credentials["auth_url"],
+                          service_type="compute")
+
+
+# Query the list of images for the given name, and create a VM of the matched
+# images
+def createVM(images, name):
+    nova = getNovaClient()
+
+    for image in images:
+        if image["name"] == name:
+            flavor = nova.flavors.find(name="m1.small")
+            nova.servers.create(name="exercise" + datetime.now()
+                                .microsecond, image=image, flavor=flavor)
+
+if __name__ == "__main__":
+    keystone = getKeystoneClient()
+    glance = getGlanceClient()
+
+    images = glance.images.list()
+    createVM(images, "ubuntu")
---
+++ ex1/credential.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+credentials = {
+    "username":     "admin",
+    "password":     "password",
+    "api_key":      "password",
+    "auth_url":     "http://10.0.2.15:5000/v2.0/",
+    "tenant_name":  "demo",
+    "project_id":   "demo"
+}
+
+if __name__ == "__main__":
+    pass
